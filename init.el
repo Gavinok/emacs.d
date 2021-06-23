36ded83f21de285bcb6a19787c6875cb1330c4e5
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-06-23 13:04:02 -0600

    push consult integration

--- init.el
+++ init.el
@@ -135,23 +135,19 @@
 
 ;; Setup Ivy
 (use-package counsel
-	     :config
-	     (counsel-mode 1)
-	     (global-set-key (kbd "C-s") 'swiper-isearch)
-	     (global-set-key (kbd "M-x") 'counsel-M-x)
-	     (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-	     (global-set-key (kbd "M-y") 'counsel-yank-pop)
-	     (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
-	     (global-set-key (kbd "C-c v") 'ivy-push-view)
-	     (global-set-key (kbd "C-c V") 'ivy-pop-view))
+  :config
+  (counsel-mode 1)
+  (global-set-key (kbd "C-c v") 'ivy-push-view)
+  (global-set-key (kbd "C-c V") 'ivy-pop-view))
+
+;; END COMPLETION -------------------------------------
 
 ;; Themeing
-;; todo
 (use-package badger-theme
-	     :config
-	     (load-theme 'badger t)
-	     (set-cursor-color "#dc322f")
-	     (set-face-attribute 'region nil :background "#666" :foreground "#ffffff"))
+  :config
+  (load-theme 'badger t)
+  (set-cursor-color "#dc322f")
+  (set-face-attribute 'region nil :background "#666" :foreground "#ffffff"))
 
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
 (add-to-list 'default-frame-alist '(alpha 90 90))
