92edfe2bb5725ee28d7ac684d377e238c61d7d49
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-06-29 16:57:20 -0600

    improved sudo save

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
 (defun sudo-save ()
   (interactive)
   (if (not buffer-file-name)
-      (write-file (concat "/sudo:root@localhost:" (ido-read-file-name "File:")))
+      (write-file (concat "/sudo:root@localhost:" (read-file-name "File:")))
     (write-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 ;; EVIL MODE ----------------------------------
