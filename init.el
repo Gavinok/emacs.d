61f35b111dccb4b348e8c511aff5abf09e80c3a1
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-01 19:52:42 -0600

    setup matrix client and eaf using quelpa-use-package

--- init.el
+++ init.el
@@ -546,9 +546,29 @@
 			 (:name "Messages with images"
 				:query "mime:image/*" :key 112))))
 
+;; (use-package quelpa-use-package)
 ;; (use-package matrix-client
+;;   :if (not gv/is-termux)
 ;;   :quelpa (matrix-client :fetcher github :repo "alphapapa/matrix-client.el"
 ;;                          :files (:defaults "logo.png" "matrix-client-standalone.el.sh")))
+;; (use-package eaf
+;;   :if (not gv/is-termux)
+;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+;;   :init
+;;   (use-package epc :defer t :ensure t)
+;;   (use-package ctable :defer t :ensure t)
+;;   (use-package deferred :defer t :ensure t)
+;;   (use-package s :defer t :ensure t)
+;;   (use-package eaf-evil :ensure nil)
+;;   :custom
+;;   (eaf-browser-continue-where-left-off t)
+;;   :config
+;;   (eaf-setq eaf-browser-default-search-engine "duckduckgo")
+;;   (eaf-setq eaf-browser-enable-adblocker "true")
+;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+;;   (eaf-bind-key nil "M-q" eaf-browser-keybinding))
 
 ;;; BETTER PDFS
 ;https://github.com/politza/pdf-tools
