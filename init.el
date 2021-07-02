7f4af9631ffa92398653e77e427b626b6400d628
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-01 19:51:39 -0600

    improved defaults

--- init.el
+++ init.el
@@ -365,9 +365,10 @@
   :ensure nil
   :config
   (set-frame-font "RobotoMono Nerd Font 14" nil t)
+  (setq delete-by-moving-to-trash t)
+  (setq backup-by-copying t)
   (setq backup-inhibited t)
   (setq make-backup-files nil)
-  (setq backup-by-copying t)
   (setq create-lockfiles nil)
   (setq auto-save-default nil)
   (setq inhibit-startup-screen t)
@@ -401,8 +402,17 @@
   (add-hook 'emacs-lisp-mode-hook 'prettify-symbols-mode)
   ;; Vim like scrolling
   (setq scroll-step            1
-	scroll-conservatively  10000))
-;;; dired
+	scroll-conservatively  10000)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+	'(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+(use-package savehist
+  :init
+  (savehist-mode t))
+
 ;;; EXTRA UI
 (use-package beacon
   :init
