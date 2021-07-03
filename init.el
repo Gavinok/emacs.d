2ca814deb03d42af6935010eb6959dd1c3d86466
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-02 20:24:06 -0600

    removed un used agenda view

--- init.el
+++ init.el
@@ -203,34 +203,7 @@
 	  ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
 	   ((org-agenda-overriding-header "Low Effort Tasks")
 	    (org-agenda-max-todos 20)
-	    (org-agenda-files org-agenda-files)))
-	  ("w" "Workflow Status"
-	   ((todo "WAIT"
-		  ((org-agenda-overriding-header "Waiting on External")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "REVIEW"
-		  ((org-agenda-overriding-header "In Review")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "PLAN"
-		  ((org-agenda-overriding-header "In Planning")
-		   (org-agenda-todo-list-sublevels nil)
-		   (org-agenda-files org-agenda-files)))
-	    (todo "BACKLOG"
-		  ((org-agenda-overriding-header "Project Backlog")
-		   (org-agenda-todo-list-sublevels nil)
-		   (org-agenda-files org-agenda-files)))
-	    (todo "READY"
-		  ((org-agenda-overriding-header "Ready for Work")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "ACTIVE"
-		  ((org-agenda-overriding-header "Active Projects")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "COMPLETED"
-		  ((org-agenda-overriding-header "Completed Projects")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "CANCELLED"
-		  ((org-agenda-overriding-header "Cancelled Projects")
-		   (org-agenda-files org-agenda-files)))))))
+	    (org-agenda-files org-agenda-files)))))
 
   (setq org-capture-templates
 	'(("t" "Todo" entry (file "~/Documents/org/refile.org")
