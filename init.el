a8cc49107eea027835a7052306cd88503c37e3cc
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-07 17:02:38 -0600

    faster startup settings

--- init.el
+++ init.el
@@ -13,6 +13,14 @@
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
 
+;;; Startup
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
 ;;; ASYNC
 
 ;;;; Emacs look SIGNIFICANTLY less often which is a good thing.
