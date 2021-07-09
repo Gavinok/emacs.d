b9df09a33f3110013dc59afa7e1e3cb51b8a8502
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-08 21:03:18 -0600

    relocated emacs settings for vertico

--- init.el
+++ init.el
@@ -145,8 +145,14 @@
 ;;; COMPLETION
 (use-package vertico
   :init
-  (vertico-mode))
-
+  (vertico-mode)
+  :config
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+	'(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
 (use-package orderless
   :custom (completion-styles '(orderless)))
 
@@ -497,12 +503,6 @@
   ;; Vim like scrolling
   (setq scroll-step            1
 	scroll-conservatively  10000)
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-	'(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t)
   ;;;; Show All These In The Same Window
   (add-to-list 'same-window-buffer-names "*SQL*")
   (add-to-list 'same-window-buffer-names "*Help*")
