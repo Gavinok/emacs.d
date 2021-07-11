f16b8e88cd0c4be7301b7b6c48e317dd630a67d0
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-10 22:15:37 -0600

    Maybe I will start using org-roam

--- init.el
+++ init.el
@@ -360,6 +360,18 @@
   :ensure nil
   :after org)
 
+(use-package org-roam
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory (file-truename org-directory))
+  :bind (:map org-roam-mode-map
+              (("C-c r l" . org-roam)
+               ("C-c r f" . org-roam-find-file)
+               ("C-c r g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c r i" . org-roam-insert))
+              (("C-c r I" . org-roam-insert-immediate))))
 ;;; Git
 (use-package magit
   :bind ("C-c m" . magit)
