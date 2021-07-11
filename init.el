40d773789c698fbf4a195399acfec0b1ccdf19da
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-11 15:59:29 -0600

    try to catch tty escape

--- init.el
+++ init.el
@@ -95,7 +95,26 @@
   :config
   (defun my-god-mode-update-cursor-type ()
     (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
-  (add-hook 'post-command-hook #'my-god-mode-update-cursor-type))
+  (add-hook 'post-command-hook #'my-god-mode-update-cursor-type)
+  (defvar personal/fast-keyseq-timeout 200)
+
+(defun personal/-tty-ESC-filter (map)
+  (if (and (equal (this-single-command-keys) [?\e])
+           (sit-for (/ personal/fast-keyseq-timeout 1000.0)))
+      [escape] map))
+
+(defun personal/-lookup-key (map key)
+  (catch 'found
+    (map-keymap (lambda (k b) (if (equal key k) (throw 'found b))) map)))
+
+(defun personal/catch-tty-ESC ()
+  "Setup key mappings of current terminal to turn a tty's ESC into `escape'."
+  (when (memq (terminal-live-p (frame-terminal)) '(t pc))
+    (let ((esc-binding (personal/-lookup-key input-decode-map ?\e)))
+      (define-key input-decode-map
+        [?\e] `(menu-item "" ,esc-binding :filter personal/-tty-ESC-filter)))))
+
+(personal/catch-tty-ESC))
 
 (use-package ciel
   :bind (("C-c C-i" . ciel-ci)
