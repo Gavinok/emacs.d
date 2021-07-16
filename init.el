e068811e22502551a6cd04be0bc8fa8826194c25
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-15 21:50:34 -0600

    eaf is now my go to browser

--- init.el
+++ init.el
@@ -854,25 +854,25 @@ Containing LEFT, and RIGHT aligned respectively."
   (unless (server-running-p)
     (server-start)))
 ;;; Extras
-;; (use-package quelpa-use-package)
-;; (use-package eaf
-;;   :unless gv/is-termux
-;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-;;   :init
-;;   (use-package epc :defer t :ensure t)
-;;   (use-package ctable :defer t :ensure t)
-;;   (use-package deferred :defer t :ensure t)
-;;   (use-package s :defer t :ensure t)
-;;   (use-package eaf-evil :ensure nil)
-;;   :custom
-;;   (eaf-browser-continue-where-left-off t)
-;;   :config
-;;   (eaf-setq eaf-browser-default-search-engine "duckduckgo")
-;;   (eaf-setq eaf-browser-enable-adblocker "true")
-;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-;;   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-;;   (eaf-bind-key nil "M-q" eaf-browser-keybinding))
+(use-package quelpa-use-package)
+(use-package eaf
+  :unless gv/is-termux
+  :defer t
+  :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+  :init
+  (use-package epc :defer t :ensure t)
+  (use-package ctable :defer t :ensure t)
+  (use-package deferred :defer t :ensure t)
+  (use-package s :defer t :ensure t)
+  :custom
+  (eaf-browser-continue-where-left-off t)
+  :config
+  (setq eaf-browser-default-search-engine "duckduckgo")
+  (setq eaf-browser-enable-adblocker "true")
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding))
 
 ;;;; Better PDFs
 ;https://github.com/politza/pdf-tools
