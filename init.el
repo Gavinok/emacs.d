e43e10ebc68da628fce7ce0090f8b44baa868560
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-15 21:37:01 -0600

    use markring as a jumplist

--- init.el
+++ init.el
@@ -104,6 +104,18 @@
 	 ;; I don't use god-mode in the minibuffer
 	 ("<escape>" . 'keyboard-escape-quit))
   :config
+;;;; Mark Ring
+    (defun unpop-to-mark-command ()
+    "Unpop off mark ring. Does nothing if mark ring is empty."
+    (interactive)
+    (when mark-ring
+      (let ((pos (marker-position (car (last mark-ring)))))
+	(if (not (= (point) pos))
+	    (goto-char pos)
+	  (setq mark-ring (cons (copy-marker (mark-marker)) mark-ring))
+	  (set-marker (mark-marker) pos)
+	  (setq mark-ring (nbutlast mark-ring))
+	  (goto-char (marker-position (car (last mark-ring))))))))
 ;;;; Cursor shape
   (defun my-god-mode-update-cursor-type ()
     (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
