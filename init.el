0ae8ff4ad36f15324f7abb4da11343c23536cf0b
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-07-23 22:09:23 -0600

    infer tabs vs space

--- init.el
+++ init.el
@@ -648,7 +648,15 @@
   :config
   (add-hook 'emacs-lisp-mode-hook 'prettify-symbols-mode)
   (add-hook 'prog-mode-hook 'outline-minor-mode)
-  (add-hook 'prog-mode-hook 'hs-minor-mode))
+  (add-hook 'prog-mode-hook 'hs-minor-mode)
+  (defun infer-indentation-style ()
+    ;; if our source file uses tabs, we use tabs, if spaces spaces, and if
+    ;; neither, we use the current indent-tabs-mode
+    (let ((space-count (how-many "^  " (point-min) (point-max)))
+          (tab-count (how-many "^\t" (point-min) (point-max))))
+      (if (> space-count tab-count) (setq indent-tabs-mode nil))
+      (if (> tab-count space-count) (setq indent-tabs-mode t))))
+  (add-hook 'prog-mode-hook 'infer-indentation-style))
 
 ;;; EXTRA UI
 ;;;; Beacon
