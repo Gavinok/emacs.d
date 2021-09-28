136b8b0128a2df7011b2076303fb825f52313a77
Lanre Oreyomi <lanre.oreyomi@nokia.com>
2021-09-28 10:04:14 -0400

    remove lsp support

--- init.el
+++ init.el
@@ -412,20 +412,20 @@ Version 2017-01-11"
 ;;     (setq meghanada-java-path "java")
 ;;     (setq meghanada-maven-path "mvn"))))
 
-(progn
-  (use-package projectile)
-  (use-package flycheck)
-  (use-package yasnippet :config (yas-global-mode))
-  (use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
-    :config (setq lsp-completion-enable-additional-text-edit nil))
-  (use-package hydra)
-  (use-package company)
-  (use-package lsp-ui)
-  (use-package which-key :config (which-key-mode))
-  (use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-  ;; (use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
-  ;; (use-package dap-java :ensure nil)
-  (use-package consult-lsp))
+;; (progn
+;;   (use-package projectile)
+;;   (use-package flycheck)
+;;   (use-package yasnippet :config (yas-global-mode))
+;;   (use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
+;;     :config (setq lsp-completion-enable-additional-text-edit nil))
+;;   (use-package hydra)
+;;   (use-package company)
+;;   (use-package lsp-ui)
+;;   (use-package which-key :config (which-key-mode))
+;;   (use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+;;   ;; (use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+;;   ;; (use-package dap-java :ensure nil)
+;;   (use-package consult-lsp))
 
 (use-package haskell-mode
   :ensure t
