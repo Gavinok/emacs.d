a7139f934e1cec8817cffa5134fa20aa0e93a84e
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-09-27 23:28:50 -0600

    relocated mu4e config to a separate file

--- init.el
+++ init.el
@@ -887,117 +887,6 @@ Version 2017-01-11"
   ;; emacs 28
   (setq dired-kill-when-opening-new-dired-buffer t))
 
-;;; EMAIL
-(use-package mu4e
-  :unless gv/is-termux
-  :load-path "/usr/share/emacs/site-lisp/mu4e"
-  :ensure nil
-  :bind (("C-x M" . mu4e)
-         ("C-x m" . mu4e-compose-new)
-         ([remap mu4e~headers-jump-to-maildir] . my/jump-to-maildir))
-  :commands (mu4e mu4e-compose-new)
-  :init
-  ;; set *before* loading mu4e; and restart emacs if you want to change it
-  ;; users of use-packag~ should can use the :init section for this.
-  ;; (setq mu4e-view-use-old t)
-  ;; Show full email address
-  (setq mu4e-view-show-addresses 't
-        mu4e-maildir "~/.local/share/mail"
-        ;; where to put attachemnts
-        mu4e-attachment-dir  "~/Downloads"
-        ;; use mu4e for e-mail in emacs
-        mail-user-agent 'mu4e-user-agent)
-  :config
-  (defun my/jump-to-maildir ()
-    (interactive)
-    (let ((maildir (completing-read "Maildir: " (mu4e-get-maildirs))))
-      (mu4e-headers-search (format "maildir:\"%s\"" maildir))))
-  ;; This is set to 't' to avoid mail syncing issues when using mbsync
-  (setq mu4e-change-filenames-when-moving t)
-
-  ;; This prevents saving the email to the Sent folder since gmail will do this for us on their end.
-  (setq mu4e-sent-messages-behavior 'delete
-        message-kill-buffer-on-exit t)
-
-;;;; Attachment reminders
-  (use-package message-attachment-reminder)
-;;;; Org In Emails
-  (use-package org-mime
-    :config
-    (setq org-mime-export-options '(:section-numbers nil
-                                                     :with-author nil
-                                                     :with-toc nil)))
-;;;; mbsync and msmtp setup
-  (setq mu4e-get-mail-command "mailsync"
-        sendmail-program "/usr/bin/msmtp"
-        message-sendmail-extra-arguments '("--read-envelope-from")
-        send-mail-function 'smtpmail-send-it
-        message-sendmail-f-is-evil t
-        message-send-mail-function 'message-send-mail-with-sendmail)
-
-  ;;images in emails
-  (setq mu4e-view-show-images t)
-;;;; Abbreviations
-  (define-abbrev-table 'mu4e-compose-mode-abbrev-table
-    '(("sin" "Sincerely, \nGavin Jaeger-Freeborn" nil 1)))
-
-  (add-hook 'mu4e-compose-mode-hook #'abbrev-mode)
-  (add-to-list 'auto-mode-alist '("^/tmp/neomutt.*\\'" . mail-mode)) ;; neomutt
-  (add-to-list 'auto-mode-alist '(".*snd\.[0-9].*\\'" . mail-mode))  ;; mblaze
-  (add-hook 'mu4e-view-mode-hook 'visual-line-mode)
-
-;;;; Accounts
-  (setq mu4e-contexts
-        (list
-         ;; Personal account
-         (make-mu4e-context
-          :name "Personal"
-          :match-func
-          (lambda (msg)
-            (when msg
-              (string-prefix-p "/personal" (mu4e-message-field msg :maildir))))
-          :vars '((user-mail-address                . "gavinfreeborn@gmail.com")
-                  (user-full-name                   . "Gavin Jaeger-Freeborn")
-                  (mu4e-drafts-folder               . "/[Gmail].Drafts")
-                  (mu4e-sent-folder                 . "/[Gmail].Sent Mail")
-                  (mu4e-refile-folder               . "/[Gmail].All Mail")
-                  (mu4e-trash-folder                . "/[Gmail].Trash")))))
-;;;; Contacts
-  (setq mail-personal-alias-file  "~/.config/mutt/aliases")
-  (setq mu4e-org-contacts-file  (concat org-directory "/contacts.org"))
-  (add-to-list 'mu4e-headers-actions
-               '("org-contact-add" . mu4e-action-add-org-contact) t)
-  (add-to-list 'mu4e-view-actions
-               '("org-contact-add" . mu4e-action-add-org-contact) t)
-;;;; Bookmarks
-  (setq mu4e-bookmarks '((:name "Main INBOX"
-                                :query "maildir:\"/INBOX\"" :key 115)
-                         (:name "To Handle"
-                                :query "((flag:flagged AND (NOT flag:replied)) OR (NOT flag:seen))" :key 116)
-                         (:name "Today's messages"
-                                :query "date:today..now" :key 118)
-                         (:name "Last 7 days"
-                                :query "date:7d..now" :hide-unread t :key 119)
-                         (:name "Messages with images"
-                                :query "mime:image/*" :key 112))) ; email client depends on mu command
-
-;;;; Headers View
-  (set-face-attribute 'mu4e-flagged-face nil
-                      :background "#900")
-  (set-face-attribute 'mu4e-flagged-face nil
-                      :foreground "#000")
-  (set-face-attribute 'mu4e-unread-face nil
-                      :background "#900")
-  (set-face-attribute 'mu4e-unread-face nil
-                      :foreground "#000")
-  (setq-default mu4e-use-fancy-chars t)
-  (setq-default mu4e-header-sort-field :date)
-  (setq-default mu4e-headers-show-threads nil)
-  (setq-default mu4e-headers-fields '((:flags         .    6)
-                                      (:from-or-to    .   22)
-                                      (:subject       .   70)
-                                      (:human-date    .   nil)))
-  (mu4e-hide-other-mu4e-buffers))
 ;;; PASS
 (use-package password-store
   :commands (password-store-copy
@@ -1151,6 +1040,9 @@ Containing LEFT, and RIGHT aligned respectively."
   (setq proced-auto-update-flag t
         proced-auto-update-interval 2
         proced-decend t))
+
+;;; mu4e
+(load "~/.emacs.d/lisp/mu4e-config.el")
 
 ;;; EXWM
 (use-package exwm
