072d320fc0012bc6a1ff3bdd91041fc93cfacc7b
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-11-16 09:26:18 -0700

    better garbage collection

--- init.el
+++ init.el
@@ -2,10 +2,10 @@
 ;; Minimize garbage collection during startup
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; Lower threshold back to 8 MiB (default is 800kB)
+;; Lower threshold to speed up garbage collection
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (expt 2 23))))
+            (setq gc-cons-threshold (* 2 1000 1000))))
 ;;; PACKAGE LIST
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
