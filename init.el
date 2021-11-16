ad498eb42640f51b88b3cbaae0dac500ef38a638
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-11-16 09:53:54 -0700

    started using embark for like the 3rd time

--- init.el
+++ init.el
@@ -271,6 +271,35 @@ Version 2017-01-11"
   ;; Enable recursive minibuffers
   (setq enable-recursive-minibuffers t))
 
+(use-package embark
+  :ensure t
+  :bind
+  (("C-=" . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  (setq embark-prompter 'embark-completing-read-prompter)
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  ;; (add-to-list 'display-buffer-alist
+  ;;              '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+  ;;                nil
+  ;;                (window-parameters (mode-line-format . none))))
+  )
+
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 ;;; THEMEING
 (use-package ujelly-theme
   :config
