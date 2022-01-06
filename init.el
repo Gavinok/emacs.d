1788d9db9eabc32bb03ad16cdffe84ba3a6c2460
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-01-06 01:57:16 -0700

    trying out avy

--- init.el
+++ init.el
@@ -240,6 +240,27 @@
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
+(use-package avy
+  :ensure
+  :bind ("M-j" . avy-goto-char-timer)
+  :config
+  (defun avy-action-mark-to-char (pt)
+    (activate-mark)
+    (goto-char pt))
+
+  (setf (alist-get ?  avy-dispatch-alist) 'avy-action-mark-to-char)
+  (defun avy-action-embark (pt)
+    (unwind-protect
+        (save-excursion
+          (goto-char pt)
+          (embark-act))
+      (select-window
+       (cdr (ring-ref avy-ring 0))))
+    t)
+
+  (setf (alist-get ?. avy-dispatch-alist) 'avy-action-embark)
+  )
+
 ;;; THEMEING
 (use-package modus-themes
   :config
