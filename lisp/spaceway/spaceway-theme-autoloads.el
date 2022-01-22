6307b89767b37cd0c57926bc18159e2b55b23cf7
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-01-22 11:49:54 -0700

    Added custom spaceway theme

---
+++ lisp/spaceway/spaceway-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; spaceway-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "spaceway-theme" "spaceway-theme.el" (0 0 0 0))
+;;; Generated autoloads from spaceway-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "spaceway-theme" '("spaceway")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; spaceway-theme-autoloads.el ends here
---
+++ lisp/spaceway/spaceway-theme-pkg.el
@@ -0,0 +1,7 @@
+;;; Generated package description from spaceway-theme.el  -*- no-byte-compile: t -*-
+(define-package "spaceway-theme" "20180214.1624" "Spaceway theme
+for GNU Emacs 24 (deftheme)" 'nil
+:commit "bf724ce7806a738d2043544061e5f9bbfc56e674" :authors
+'(("Mark Tran" . "mark.tran@gmail.com")) :maintainer '("Mark
+Tran" . "mark.tran@gmail.com")
+:url "http://github.com/marktran/color-theme-spaceway")
