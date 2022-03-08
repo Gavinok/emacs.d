630e0f2a4a90155127ff532277f02cc77db743a6
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-03-08 09:41:35 -0700

    evil mode optionaly added

--- init.el
+++ init.el
@@ -665,8 +665,31 @@
   (setq evil-split-window-right t)
   (setq evil-undo-system 'undo-fu)
   :config
-  (evil-mode 1)
+  ;; (evil-mode 1)
   (evil-set-leader 'normal " "))
+
+(defun my-change-number-at-point (change increment)
+  (let ((number (number-at-point))
+        (point (point)))
+    (when number
+      (progn
+        (forward-word)
+        (search-backward (number-to-string number))
+        (replace-match (number-to-string (funcall change number increment)))
+        (goto-char point)))))
+
+(defun my-increment-number-at-point (&optional increment)
+  "Increment number at point like vim's C-a"
+  (interactive "p")
+  (my-change-number-at-point '+ (or increment 1)))
+
+(defun my-decrement-number-at-point (&optional increment)
+  "Decrement number at point like vim's C-x"
+  (interactive "p")
+  (my-change-number-at-point '- (or increment 1)))
+
+(global-set-key (kbd "C-x n a") 'my-increment-number-at-point)
+(global-set-key (kbd "C-x n x") 'my-decrement-number-at-point)
 
 (use-package evil-smartparens
   :hook (smartparens-mode . evil-smartparens-mode))
