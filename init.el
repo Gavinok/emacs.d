b1853770e70a2cc03e758b30fa4764702ff80d56
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-04-30 11:37:26 -0600

    Improvements to lisp workflow

--- init.el
+++ init.el
@@ -432,7 +432,7 @@
           (magit-status (project-root (project-current t)) "Magit" m)))
 
   (defvar project-root-markers
-    '(".git" "package.clj" "package.json" "mix.exs" "Project.toml" ".project" "Cargo.toml"))
+    '(".git" "package.clj" "package.json" "mix.exs" "Project.toml" ".project" "Cargo.toml" "qlfile"))
 
   (defun gv/project-find-root (path)
     (let* ((this-dir (file-name-as-directory (file-truename path)))
@@ -900,15 +900,17 @@ In Transient Mark mode, activate mark if optional third arg ACTIVATE non-nil."
   :commands (sly sly-connect)
   :init
   (setq inferior-lisp-program "sbcl")
-  (setq sly-default-lisp 'ccl)
+  (setq sly-default-lisp 'roswell)
+  (setq ros-config (concat user-emacs-directory
+                                  "ros-conf.lisp"))
   (setq sly-lisp-implementations
-        `((ccl ("ccl") :coding-system utf-8-unix)
-          (sbcl ("sbcl") :coding-system utf-8-unix)
+        `((sbcl ("sbcl") :coding-system utf-8-unix)
+          (ccl ("ccl") :coding-system utf-8-unix)
           (ecl ("ecl") :coding-system utf-8-unix)
-          (roswell ("ros" "-Q" "run"))
-          (roswell-sbcl ("ros" "-L" "sbcl" "-Q" "-l" "~/.sbclrc" "run") :coding-system utf-8-unix)
-          (qlot ("qlot" "exec" "ros" "run" "-S" ".")
+          (roswell ("ros" "-Q" "-l" ,ros-config "run"))
+          (qlot ("qlot" "exec" "ros" "-l" ,ros-config "run" "-S" ".")
                 :coding-system utf-8-unix)))
+
   (defun qlot-sly ()
     "Start a sly repl using qlot at the projects root"
     (interactive)
@@ -916,6 +918,13 @@ In Transient Mark mode, activate mark if optional third arg ACTIVATE non-nil."
       (if (cd dir)
           (sly 'qlot)
         (error (format "Failed to cd to %s" dir)))))
+
+  (defun sly-critique-file ()
+    "Lint this file with lisp-critic"
+    (interactive)
+    (sly-eval-async '(ql:quickload :lisp-critic))
+    (sly-eval-async `(lisp-critic:critique-file ,(buffer-file-name))))
+
   (defun gv/connect-to-stumpwm ()
     (interactive)
     (start-process-shell-command "stumpish start-slynk" nil
