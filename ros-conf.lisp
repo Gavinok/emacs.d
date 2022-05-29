773cf59b4ba740fcb9d182ee2c8a746244b36332
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-05-29 09:34:49 -0700

    Don't let my/add-to-local-project duplicate directories

--- ros-conf.lisp
+++ ros-conf.lisp
@@ -4,10 +4,10 @@ This is just the config I have roswell load before every repl
 (setq *print-case* :downcase)
 (declaim (optimize (speed 3) (debug 3) (safety 3)))
 (defun my/table (&rest key-and-value &key &allow-other-keys)
-    (let ((new-table (make-hash-table)))
-      (loop for (sym num) on key-and-value by #'cddr
-            do (setf (gethash sym new-table) num))
-      new-table))
+  (let ((new-table (make-hash-table)))
+    (loop for (sym num) on key-and-value by #'cddr
+          do (setf (gethash sym new-table) num))
+    new-table))
 
 (defun my/add-to-local-project ()
-    (push (uiop:getcwd) ql:*local-project-directories*))
+  (pushnew (uiop:getcwd) ql:*local-project-directories*))
