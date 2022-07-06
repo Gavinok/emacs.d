7121fcaa06c58d5464151c4605b717fc9d6f69c6
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-07-06 13:51:14 -0700

    basic org preset changes

--- lisp/org-config.el
+++ lisp/org-config.el
@@ -19,6 +19,7 @@
          :map org-mode-map
          ("C-M-i" . completion-at-point))
   :config
+  (setq org-startup-indented t)
   (setq my/org-latex-scale 1.75)
   (setq org-format-latex-options
         (plist-put org-format-latex-options :scale my/org-latex-scale))
@@ -41,7 +42,7 @@
         org-edit-src-content-indentation 2
         org-hide-block-startup nil
         org-src-preserve-indentation nil
-        org-startup-folded 'content
+        org-startup-folded 'fold
         org-cycle-separator-lines 2
         org-hide-leading-stars t
         org-export-backends '(markdown ascii html icalendar latex odt)
@@ -65,9 +66,9 @@
   (setq org-babel-lisp-eval-fn #'sly-eval)
   (org-babel-do-load-languages
    'org-babel-load-languages
-   '((haskell . t) (emacs-lisp . t) (shell . t)
+   '((haskell . t) (emacs-lisp . t) (shell . t) (python . t)
      (C . t) (lua . t) (dot . t) (java . t)
-     (lisp . t) (clojure . t)))
+     (lisp . t) (clojure . t) (scheme . t)))
   (use-package ob-async
     :ensure t)
   (use-package ob-rust
@@ -123,10 +124,17 @@
             ("k" "Knowledge" entry (file+olp (lambda () (concat org-directory "/archive.org")) "Knowledge")
              "* %?\nEntered on %U\n  %i\n  %a")
             ("s" "Scheduled Event")
-            ("sm" "Meeting" entry (file+headline (lambda () (concat org-directory "/Work.org")) "Meetings")
+            ("sm" "Meeting" entry (file+headline (lambda () (concat org-directory "/Work.org"))
+                                                 "Meetings")
+             "* Meeting with  %? :MEETING:\nSCHEDULED: %T\n:PROPERTIES:
+:LOCATION: %^{location|Anywhere|Home|Work|School}
+:END:")
+            ("se" "Event" entry (file+headline (lambda () (concat org-directory "/Work.org"))
+                                               "Events")
              "* Meeting with  %?\nSCHEDULED: %T\n")
-            ("se" "Event" entry (file+headline (lambda () (concat org-directory "/Work.org")) "Meetings")
-             "* Meeting with  %?\nSCHEDULED: %T\n")
+            ("st" "Time Block" entry (file+headline (lambda () (concat org-directory "/Work.org"))
+                                                    "Time Blocks")
+             "* Work On %?\nSCHEDULED: %T\n")
             ("M" "movie" entry (file+headline (lambda () (concat org-directory "/mylife.org")) "Movies to Watch")
              "* %?\n")
             ("v" "Video Idea" entry (file+olp (lambda () (concat org-directory "/youtube.org"))
@@ -185,10 +193,10 @@
 (use-package org-modern
   :commands (org-modern-mode org-modern-agenda)
   :init
+  (setq org-modern-todo t
+        org-modern-variable-pitch nil)
   (add-hook 'org-mode-hook #'org-modern-mode)
   (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
-  (setq org-modern-todo nil
-        org-modern-variable-pitch nil)
   (global-org-modern-mode))
 
 
@@ -199,90 +207,8 @@
   :config
   (add-to-list 'org-structure-template-alist '("sh"  . "src sh"))
   (add-to-list 'org-structure-template-alist '("el"  . "src emacs-lisp"))
-  (add-to-list 'org-structure-template-alist '("vim"  . "src vim"))
-  (add-to-list 'org-structure-template-alist '("cpp"  . "src C++ :includes <iostream>  :namespaces std")))
-
-(use-package svg-tag-mode
-  :config
-  (defconst date-re "[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}")
-  (defconst time-re "[0-9]\\{2\\}:[0-9]\\{2\\}")
-  (defconst day-re "[A-Za-z]\\{3\\}")
-  (defconst day-time-re (format "\\(%s\\)? ?\\(%s\\)?" day-re time-re))
-
-  (defun svg-progress-percent (value)
-    (svg-image (svg-lib-concat
-                (svg-lib-progress-bar (/ (string-to-number value) 100.0)
-                                      nil :margin 0 :stroke 2 :radius 3 :padding 2 :width 11)
-                (svg-lib-tag (concat value "%")
-                             nil :stroke 0 :margin 0)) :ascent 'center))
-
-  (defun svg-progress-count (value)
-    (let* ((seq (mapcar #'string-to-number (split-string value "/")))
-           (count (float (car seq)))
-           (total (float (cadr seq))))
-      (svg-image (svg-lib-concat
-                  (svg-lib-progress-bar (/ count total) nil
-                                        :margin 0 :stroke 2 :radius 3 :padding 2 :width 11)
-                  (svg-lib-tag value nil
-                               :stroke 0 :margin 0)) :ascent 'center)))
-
-  (setq svg-tag-tags
-        `(
-          ;; Org tags
-          (":\\([A-Za-z0-9]+\\)" . ((lambda (tag) (svg-tag-make tag))))
-          (":\\([A-Za-z0-9]+[ \-]\\)" . ((lambda (tag) tag)))
-          
-          ;; Task priority
-          ("\\[#[A-Z]\\]" . ( (lambda (tag)
-                                (svg-tag-make tag :face 'org-priority 
-                                              :beg 2 :end -1 :margin 0))))
-
-          ;; Progress
-          ("\\(\\[[0-9]\\{1,3\\}%\\]\\)" . ((lambda (tag)
-                                              (svg-progress-percent (substring tag 1 -2)))))
-          ("\\(\\[[0-9]+/[0-9]+\\]\\)" . ((lambda (tag)
-                                            (svg-progress-count (substring tag 1 -1)))))
-          
-          ;; TODO / DONE
-          ("TODO" . ((lambda (tag) (svg-tag-make "TODO" :face 'org-todo :inverse t :margin 0))))
-          ("DONE" . ((lambda (tag) (svg-tag-make "DONE" :face 'org-done :margin 0))))
-
-
-          ;; Citation of the form [cite:@Knuth:1984] 
-          ("\\(\\[cite:@[A-Za-z]+:\\)" . ((lambda (tag)
-                                            (svg-tag-make tag
-                                                          :inverse t
-                                                          :beg 7 :end -1
-                                                          :crop-right t))))
-          ("\\[cite:@[A-Za-z]+:\\([0-9]+\\]\\)" . ((lambda (tag)
-                                                     (svg-tag-make tag
-                                                                   :end -1
-                                                                   :crop-left t))))
-
-          
-          ;; Active date (with or without day name, with or without time)
-          (,(format "\\(<%s>\\)" date-re) .
-           ((lambda (tag)
-              (svg-tag-make tag :beg 1 :end -1 :margin 0))))
-          (,(format "\\(<%s \\)%s>" date-re day-time-re) .
-           ((lambda (tag)
-              (svg-tag-make tag :beg 1 :inverse nil :crop-right t :margin 0))))
-          (,(format "<%s \\(%s>\\)" date-re day-time-re) .
-           ((lambda (tag)
-              (svg-tag-make tag :end -1 :inverse t :crop-left t :margin 0))))
-
-          ;; Inactive date  (with or without day name, with or without time)
-          (,(format "\\(\\[%s\\]\\)" date-re) .
-           ((lambda (tag)
-              (svg-tag-make tag :beg 1 :end -1 :margin 0 :face 'org-date))))
-          (,(format "\\(\\[%s \\)%s\\]" date-re day-time-re) .
-           ((lambda (tag)
-              (svg-tag-make tag :beg 1 :inverse nil :crop-right t :margin 0 :face 'org-date))))
-          (,(format "\\[%s \\(%s\\]\\)" date-re day-time-re) .
-           ((lambda (tag)
-              (svg-tag-make tag :end -1 :inverse t :crop-left t :margin 0 :face 'org-date))))))
-
-  (svg-tag-mode t))
+  (add-to-list 'org-structure-template-alist '("vim" . "src vim"))
+  (add-to-list 'org-structure-template-alist '("cpp" . "src C++ :includes <iostream>  :namespaces std")))
 
 (use-package org-transclusion
   :after org
