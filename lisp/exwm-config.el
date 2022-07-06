b0ffe6bcda289394f5679b2424e001dbc990ef9a
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-07-06 13:28:31 -0700

    exwm clean up

--- lisp/exwm-config.el
+++ lisp/exwm-config.el
@@ -2,23 +2,13 @@
   :unless my/is-termux
   :ensure t
   :init
-  (setq exwm-workspace-number 4)
+  (setq exwm-workspace-number 2)
   :config
   (setq exwm-manage-force-tiling t)
  ;;;; Hooks
   (add-hook 'exwm-update-class-hook
             (lambda ()
               (exwm-workspace-rename-buffer exwm-class-name)))
-
-    ;; (add-hook 'exwm-manage-finish-hook
-    ;;         (lambda ()
-    ;;           (when (and exwm-class-name
-    ;;                   (or (string= exwm-class-name "qutebrowser")
-    ;;                       (string= exwm-class-name "libreoffice-writer")
-    ;;                       (string= exwm-class-name "libreoffice-calc")
-    ;;                       (string= exwm-class-name "Google-chrome")
-    ;;                       (string= exwm-class-name "Brave-browser")))
-    ;;          (exwm-input-set-local-simulation-keys nil))))
 
     (add-hook 'exwm-update-title-hook
             (lambda ()
@@ -27,29 +17,12 @@
                 ("libreoffice-writer" (exwm-workspace-rename-buffer (format "Writer: %s" exwm-title)))
                 ("libreoffice-calc" (exwm-workspace-rename-buffer (format "Calc: %s" exwm-title)))
                 ("St" (exwm-workspace-rename-buffer (format "%s" exwm-title))))))
-    ;; Hide the modeline on all X windows
+
+    ;; Hide the mode-line on all floating X windows
     (add-hook 'exwm-floating-setup-hook
               (lambda ()
                 (exwm-layout-hide-mode-line)))
-;;;; Funcs
-  (defun my/switch-to-scratch-and-back ()
-    "Toggle between *scratch* buffer and the current buffer.
-     If the *scratch* buffer does not exist, create it."
-    (interactive)
-    (let ((scratch-buffer-name (get-buffer-create "*scratch*")))
-      (if (equal (current-buffer) scratch-buffer-name)
-          (switch-to-buffer (other-buffer))
-        (switch-to-buffer scratch-buffer-name (lisp-interaction-mode)))))
-
-  ;;Just disallow killing of scratch buffer
-  (defun my/unkillable-scratch-buffer ()
-    (if (equal (buffer-name (current-buffer)) "*scratch*")
-        (progn
-          (delete-region (point-min) (point-max))
-          nil)
-      t))
-
-  (add-hook 'kill-buffer-query-functions 'my/unkillable-scratch-buffer)
+;;;; Functions
 ;;;; Global Key Bindings
   (add-hook 'exwm-manage-finish-hook
             (lambda ()
@@ -67,7 +40,7 @@
                                                         ([?\C-j] . [?\C-k])
                                                         ([?\C-s] . [?\C-f])
                                                         ([?\C-e] . [end])
-                                                        ([?\C-E] . [?\C-e])
+                                                        (,(kbd "C-S-E") . [?\C-e])
                                                         ([?\M-v] . [prior])
                                                         ([?\C-v] . [next])
 							([?\C-d] . [delete])
@@ -85,26 +58,38 @@ back to switching frames."
              (funcall #',fn 1)
            (error (other-frame 1))))))
 
+  (defmacro my/window-swap (direction)
+    "Creates a function for changing the focused window but falls
+back to switching frames."
+    (let ((fn (intern (concat "windmove-swap-states-" (symbol-name direction)))))
+      `(lambda (&optional arg) (interactive)
+         (condition-case nil
+             (funcall #',fn)
+           (error (other-frame 1))))))
+
   (defmacro my/exwm-run (command)
     "Returns a function that calls the given command"
-    `(lambda ()
+    `(lambda (&optional arg)
        (interactive)
        (start-process-shell-command ,command nil ,command)))
 
   (setq exwm-input-global-keys
+        ;; Window Managment
         `((,(kbd "s-SPC") . ,(my/exwm-run "cabl -c"))
           ([?\s-h] . ,(my/window-switch left))
           ([?\s-l] . ,(my/window-switch right))
           ([?\s-j] . windmove-down)
           ([?\s-k] . windmove-up)
-          ;; Window Managment
+          (,(kbd "s-H") . windmove-swap-states-left)
+          (,(kbd "s-L") . ,(my/window-swap right))
+          (,(kbd "s-J") . ,(my/window-swap down))
+          (,(kbd "s-K") . ,(my/window-swap up))
           (,(kbd "<s-tab>") . other-window)
           ([?\s-v] . crux-swap-windows)
           ([?\s-o] . other-frame)
-          ;; ([?\s-f] . exwm-layout-set-fullscreen)
+          ([?\s-f] . exwm-layout-set-fullscreen)
           ([?\s-c] . inferior-octave)
           ([?\s-C] . kill-this-buffer)
-
           ;; tile exwm
           ([?\s-t] . exwm-reset)
 
@@ -135,14 +120,12 @@ back to switching frames."
   (define-key exwm-mode-map (kbd "C-q") 'exwm-input-send-next-key)
   (define-key exwm-mode-map (kbd "<s-escape>") 'exwm-input-release-keyboard)
 
-  ;; Start in char-mode
-  ;; (setq exwm-manage-configurations '((t char-mode t)))
-
 (require 'exwm)
 ;;;; Start EXWM
 ;;;; Start Programs For EXWM
 (exwm-enable))
 
+;; Broken on current version of emacs
 ;; (use-package exwm-systemtray
 ;;   :ensure nil
 ;;   :after exwm
@@ -159,7 +142,7 @@ back to switching frames."
   :demand t
   :config
   (setq exwm-randr-workspace-output-plist ;; '(3 "HDMI2")
-        '(2 "DP2"))
+        '(1 "DP2"))
   (add-hook 'exwm-randr-screen-change-hook
             (lambda ()
               (start-process-shell-command
@@ -171,16 +154,6 @@ back to switching frames."
 ;;   https://obsproject.com/forum/resources/obs-websocket-remote-control-obs-studio-from-websockets.466/
 ;; Also will need this emacs package
 ;;   https://github.com/sachac/obs-websocket-el
-(defun streaming (&optional)
-  "Setup Streaming Environment"
-  (interactive)
-  (keycast-mode 1)
-  (start-process-shell-command "obs " nil "obs")
-  (persp-new "demo")
-  (let ((spec "streaming"))
-    (persp-new spec)
-    (persp-switch spec))
-  (persp-set-buffer "obs"))
 
 
 ;;; Override Pinentry For Pass To Avoid Locking Up Emacs
