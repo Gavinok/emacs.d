0ac45c6a77fd2b735067aa2d3bedf789e85a36d4
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-07-06 13:38:11 -0700

    Complete modeline refactor

--- init.el
+++ init.el
@@ -1237,46 +1237,92 @@ In Transient Mark mode, activate mark if optional third arg ACTIVATE non-nil."
   (auth-source-pass-enable))
 
 ;;; MODELINE
-(defun simple-mode-line-render (left right)
-  "Return a string of `window-width' length.
-Containing LEFT, and RIGHT aligned respectively."
-  (let ((available-width
-         (- (window-total-width)
-            (+ (length (format-mode-line left))
-               (length (format-mode-line right))))))
-    (append left
-            (list (format (format "%%%ds" available-width) " "))
-            right)))
 (unless my/is-terminal
   (setq-default left-margin-width 2)
   (setq-default right-margin-width 2))
 (set-window-buffer nil (current-buffer))
+
+(defvar ml-text-scale-factor 1.0
+  "Scale of mode-line font size to default font size, as a float.
+This is needed to make sure that text is properly aligned.")
+
+(defun ml-fill-to-center (reserve face)
+  "Return empty space to the center, leaving RESERVE space on the right."
+  (when ml-text-scale-factor
+    (setq reserve (* ml-text-scale-factor reserve)))
+  (propertize " "
+              'display `((space :align-to (- (+ center (.5 . right-margin))
+                                             ,reserve
+                                             (.5 . left-margin))))
+              'face face))
+
+(defun ml-fill-to-right (reserve face)
+  "Return empty space, leaving RESERVE space on the right."
+  (when ml-text-scale-factor
+    (setq reserve (* ml-text-scale-factor reserve)))
+  (when (and window-system (eq 'right (get-scroll-bar-mode)))
+    (setq reserve (- reserve 2))) ; Powerline uses 3 here, but my scrollbars are narrower.
+  (propertize " "
+              'display `((space :align-to (- (+ right right-fringe right-margin)
+                                             ,reserve)))
+              'face face))
+
+(defun ml-render-2-part (left right &optional fill-face)
+  (concat left
+          (ml-fill-to-right (string-width (format-mode-line right)) fill-face)
+          right))
+
+(add-hook 'window-configuration-change-hook #'cogent-line-set-selected-window)
+(add-hook 'focus-in-hook #'cogent-line-set-selected-window)
+(add-hook 'focus-out-hook #'cogent-line-unset-selected-window)
+(advice-add 'handle-switch-frame :after #'cogent-line-set-selected-window)
+(add-hook 'window-selection-change-functions #'cogent-line-set-selected-window)
+
+;; determin the focused window
+(defvar cogent-line-selected-window (frame-selected-window))
+(defun cogent-line-set-selected-window (&rest _args)
+  (when (not (minibuffer-window-active-p (frame-selected-window)))
+    (setq cogent-line-selected-window (frame-selected-window))
+    (force-mode-line-update)))
+(defun cogent-line-unset-selected-window ()
+  (setq cogent-line-selected-window nil)
+  (force-mode-line-update))
+(defun cogent-line-selected-window-active-p ()
+  (eq cogent-line-selected-window (selected-window)))
+
 (setq-default mode-line-format
-      '((:eval
-         (format-mode-line
-          (simple-mode-line-render
-           ;; Left
-           '(" "
-             (:eval (propertize
-                     (if (and (not buffer-read-only) (buffer-modified-p))
-                         "● "
-                       "  " )
-                     'face 'error))
-             mode-line-buffer-identification
-             ;; value of current line number
-             " %l:%c"
-             (:eval (propertize
-                     (concat " %p%%" " "
-                             "「 %m 」")
-                     'face 'shadow))
-             mode-line-misc-info
-             )
-           ;; Right
-           '((:eval (propertize
-                     (format-time-string "%a, %b %d %I:%M%p")
-                     'face 'font-lock-keyword-face))
-             " "
-             "    "))))))
+              `(" "
+                ;; indicate if the buffer has been modified
+                (:eval (propertize
+                        (if (and (not buffer-read-only) (buffer-modified-p))
+                            "● " "  " )
+                        'face 'error))
+
+                ;; Buffer name (no longer than 1/3 of the screen)
+                (:eval (concat  (let ((bname (buffer-name (current-buffer)))
+                                      (max-len (/ (window-total-width) 3)))
+                                  (if (> (length bname) max-len)
+                                      (substring bname 0 max-len)
+                                    bname))))
+                ;; value of current line number
+                " "
+                " %l:%c"
+                (:eval (propertize
+                        (concat " %p%"
+                                " "
+                                "「 %m 」")
+                        'face (if (cogent-line-selected-window-active-p)
+                                  'shadow
+                                'mode-line-inactive)))
+                ;; I still have yet to come up with a better option
+                (:eval
+                 (propertize " " 'display
+	                     `((space :align-to
+			              (- (+ right right-fringe right-margin)
+			                 ,(string-width (format-mode-line mode-line-misc-info)))))))
+                mode-line-misc-info))
+
+
 
 ;;; Server Setup
 (use-package server
